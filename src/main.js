c76c4aebabfb9a227bbf970ada4228e7458db8a4
殷凌越 <yin_lingyue@163.com>
2017-07-05 16:34:04 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// APP.vue  是程序的入口大组件
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
+new Vue({//实例
+  //挂载到aap上
   el: '#app',
+  //路由 router文件夹里的内容
+  //router: router的简写
   router,
+  //引入模板是app 
   template: '<App/>',
+  //new实例的时候 外部引入一个app组件
   components: { App }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,10 @@ import Hello from '@/components/Hello'
 Vue.use(Router)
 
 export default new Router({
+	//router里面定义了一个路由routers
   routes: [
     {
+      //hello这个组件会显示APP.vue大入口组件<router-view>内
       path: '/',
       name: 'Hello',
       component: Hello
