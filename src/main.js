a9c72a6f48c8299140cbe5f49e036dd9e6afe496
wangjianying369 <412124848@qq.com>
2017-07-05 23:25:23 +0800

    index的header和swiper

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+// APP.vue 是程序的入口大组件
+import App from './App';
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueAwesomeSwiper);
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
-  router,
+  router: router,
   template: '<App/>',
   components: { App }
 })
---
+++ src/pages/index/iconswiper.vue
@@ -0,0 +1,133 @@
+<template>
+	<swiper :options="swiperOption" ref="mySwiper">
+        <swiper-slide>
+        	<div class="icon-container">
+        		<div class="icon-wrapper" v-for="item in swiperInfo.slice(8)">
+        			<img :src="item.imgUrl" class="icon-img">
+        			<p class="icon-title">{{item.title}}</p>
+        		</div>
+        	</div>
+        </swiper-slide>
+         <swiper-slide>
+            <div class="icon-container">
+                <div class="icon-wrapper" v-for="item in swiperInfo.slice(8)">
+                    <img :src="item.imgUrl" class="icon-img">
+                    <p class="icon-title">{{item.title}}</p>
+                </div>
+            </div>
+        </swiper-slide>
+        <div class="swiper-pagination icon-pagination"  slot="pagination"></div>
+    </swiper>
+</template>
+
+<script>
+
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  data () {
+    return {
+    	swiperInfo: [{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	},{
+    		imgUrl: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+    		title: "故宫",
+    		link:""
+    	}],
+     	swiperOption: {
+            autoplay: false,
+            direction : 'horizontal',
+            autoHeight: true,
+            pagination : '.swiper-pagination',
+            paginationClickable :true
+        }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+	.icon-container {
+		overflow: hidden;
+		padding-bottom: .4rem;
+	}
+	.icon-wrapper {
+		padding-top: .3rem;
+		overflow: hidden;
+		width: 25%;
+		float: left;
+	}
+	.icon-img {
+		width: .66rem;
+		display: block;
+		margin: 0 auto;
+	}
+	.icon-title {
+		margin-top: .2rem;
+		text-align: center;
+		color: #212121;
+		font-size: .28rem;
+	}
+	.icon-pagination {
+		bottom: .1rem;
+	}
+</style>
