7a740eb7446ad405419d8324948940cde8d7034c
shenglong.li <913937000@qq.com>
2017-07-05 18:34:51 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 // APP.vue 是程序的入口大组件
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   el: '#app',
