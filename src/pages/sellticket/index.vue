05032c46f9aedb4722fd6182cf76a3b01b1b10af
songtao1 <1107799536@qq.com>
2017-07-06 22:22:49 +0800

    first

--- src/pages/sellticket/index.vue
+++ src/pages/sellticket/index.vue
@@ -1,13 +1,24 @@
 <template>
-	<div>sell stick</div>
+	<promotional-tickets :ticketproductInfo = "ticketInfo"></promotional-tickets>
 </template>
 
 <script>
+import promotionaltickets from "./promotional_tickets"
 export default {
+	created:function(){
+		this.$http.get("/static/sellticket.json").then(response=>{
+			this.ticketInfo = response.body.data.ticketproductInfo;
+		}, response=>{
+			console.log("get index data error")
+		});
+	},
   data () {
     return {
-     
+     ticketInfo: []
     }
+  },
+  components:{
+  	"promotional-tickets":promotionaltickets
   }
 }
 </script>
