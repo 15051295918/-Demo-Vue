2e3e384b6267513a9445fb6f67bba25425fc24dc
hyc0422 <2510278733@qq.com>
2017-07-06 20:33:24 +0800

    change

--- src/sighthotel/index/index.vue
+++ src/sighthotel/index/index.vue
@@ -1,19 +1,45 @@
 <template>
     <div class="main">
-	123
+		<index-product :data="productInfo"></index-product>
     </div>
 </template>
 
 <script>
+import product from "./product"
 
-
+export default {
+     created: function(){
+        this.$http.get('/static/sighthotel.json').then(response => {
+           console.log(response)
+           this.productInfo = response.body.data.productInfo;
+        }, response => {
+            console.log("get index data error")
+        });
+    },
+    data() {
+      return {
+        productInfo: []
+      }
+    },
+    components: {
+        "index-product": product,
+       
+    }
+}
 
 
 </script>
 
 
 <style scoped>
-  
+  	.main {
+  		position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        min-height: 100%;
+        background: #f5f5f5;
+  	}
 
 </style>
 
