40b21a2b531c48e483d5f2a406592148ae89050f
chang986@qq.com <chang986@qq.com>
2017-07-06 17:04:16 +0800

    add city route and city.json

---
+++ src/pages/city/city.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>our citylist</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
 import List from '@/pages/list/index'
+import City from '@/pages/city/city'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
     	path: "/list",
     	name: "List",
     	component: List
+    }, {
+        path: "/city",
+        name: "City",
+        component: City
     }
   ]
 })
