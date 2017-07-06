f9155cf80a6b1809bc84545da6e4d8b5e24db8ac
wangwenli <1421247916@qq.com>
2017-07-06 16:47:39 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
+import List from '@/pages/list/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Index',
-      component: Index
+    	path: '/',
+    	name: 'Index',
+    	component: Index
+    }, {
+    	path: "/list",
+    	name: "List",
+    	component: List
     }
   ]
 })
