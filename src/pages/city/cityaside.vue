5b8e78cb12f98738414b9a4e1a5f35deb4f481c3
szz123 <18810567593@163.com>
2017-07-06 19:13:37 +0800

    change

--- src/pages/city/cityaside.vue
+++ src/pages/city/cityaside.vue
@@ -1,11 +1,61 @@
 <template>
-	<div>aside</div>
+	<div class="letter">
+		<ul>
+			<li @click="touchstart">A</li>
+			<li>B</li>
+			<li>C</li>
+			<li>D</li>
+			<li>E</li>
+			<li>F</li>
+			<li>G</li>
+			<li>H</li>
+			<li>I</li>
+			<li>J</li>
+			<li>K</li>
+			<li>L</li>
+			<li>M</li>
+			<li>N</li>
+			<li>P</li>
+			<li>Q</li>
+			<li>R</li>
+			<li>S</li>
+			<li>T</li>
+			<li>W</li>
+			<li>X</li>
+			<li>Y</li>
+			<li>Z</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-	
+export default {
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			"touchstart":function(){
+				alert()
+			}
+		}
+	}
 </script>
 
 <style scoped>
-	
+	ul{
+		position:fixed;
+		top:1rem;
+		right:0;
+	}
+	li{
+		font-size: 0.24rem;
+		width: .32rem;
+		line-height: .32rem;
+		padding-left: .2rem;
+		color:#00afc7;
+		text-align: center;
+		margin-top: .04rem
+	}
 </style>
