833f3c32ea7136a379356e68063f66a6cc432836
tianlei136092479 <136092479@qq.com>
2017-07-06 19:19:59 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
+import Oneday from '@/pages/oneday/index'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+    	path: '/onedaytour',
+    	name: 'Oneday',
+      component: Oneday
     }
   ]
 })
