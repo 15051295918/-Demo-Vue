8604427a0929e2144ef5b6268ea43cd051bc41c7
cloudpan <cloudpan08@gmail.com>
2017-07-06 20:52:15 +0800

    first

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<download></download>
+</template>
+
+<script>
+import download from './download'
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+  components: {
+        "download": download,
+        
+    }
+}
+</script>
+
+
+<style scoped>
+	
+</style>
