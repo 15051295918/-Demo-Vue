eaaa821f3c99791047c2dc614c5eabaad68ec476
bestwzl <bestwzl@sina.com>
2017-07-06 08:45:56 +0800

    change

---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    头部
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+
+
+</style>
