980fba507425374e8c9275dc751e548f312aa914
18649307981 <594402100@qq.com>
2017-07-06 22:57:10 +0800

    JayChange

--- src/pages/city/cityheader.vue
+++ src/pages/city/cityheader.vue
@@ -4,15 +4,20 @@
 		<div class="cityKey"></div>
 		<h1 class="cityheader-tit">
 			<div class="city-tab" >
-				<span class="header-city">国内</span>
-				<span class="header-city" :style="cityActive">海外</span>
+				<span class="header-city" @click="handleCountryChina" :class="{china:bgChina}">国内</span>
+				<span class="header-city" @click="handleCountrySea" :class="{sea:bgsea}">海外</span>
 			</div>			
 		</h1>
 		
 	</div>
 	<div class="header-keyword">
-		<input type="text" placeholder="输入城市名或拼音" class="city-keyword" style="text-align: center;">
+		<input v-model="value" type="text" @focus="handleInputFocus" @blur="handleInputBlur" :placeholder="placeholder" class="city-keyword" :class="{text:textalign}">
+	
 	</div>
+	<ul class="key-city-ul" v-if="list">
+		<li class="key-city-list border-bottom">大哥你在家呆着呗</li>
+	
+	</ul>
 </div>
 	
 </template>
@@ -21,12 +26,43 @@
 export default {
 	data() {
 		return {
-			cityActive : {
-					"background" : "#fff",
-					'color' : "#00afc7"
-				}
+			value : "",
+			list : false,
+			bgChina : true,
+			bgsea : false,
+			placeholder : "输入城市名或拼音",
+			textalign : true
 		}
-	}
+	},
+	methods : {
+		handleInputFocus : function(){
+				this.placeholder = "";
+				this.textalign = false;
+		},
+		handleInputBlur : function(){
+			console.log(this.placeholder)
+			this.placeholder = "输入城市名或拼音";
+			this.textalign = true;
+		},
+		handleCountryChina : function(){
+			this.bgChina = true;
+			this.bgsea = false;
+		},
+		handleCountrySea : function() {
+			this.bgChina = false;
+			this.bgsea = true;
+		}		
+	},
+	  watch: {
+	    value: function () {
+	    	if (this.value) {
+	    		this.list = true;
+	    	}else{
+	    		this.list = false;
+	    	}
+	   		
+	    }
+	  }
 }
 
 	
@@ -98,7 +134,27 @@ export default {
     -webkit-border-radius: .06rem;
     -moz-border-radius: .06rem;
     border-radius: .06rem;
-    text-align: center;
+   
 }
-
+.text{
+	text-align: center;
+}
+.china{
+	background: #fff;
+	color: #00afc7;
+}
+.sea{
+	background: #fff;
+	color: #00afc7;
+}
+.key-city-ul{
+	background: #fff
+}
+.key-city-list{
+    line-height: .76rem;
+    padding-left: .2rem;
+    font-size: .28rem;
+    color: #212121;
+    background: #fff;
+}
 </style>
