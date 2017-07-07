87f123875debcdb5a59dbe525063097f3313b37f
Milings <dhb_5_happy@163.com>
2017-07-07 08:35:59 +0800

    123123

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,18 +1,36 @@
 <template>
-	<div>加油吧，少年</div>
+	<div class="main-list">
+	  <list-header></list-header>
+    <navigation></navigation>
+	</div>
 </template>
 
 <script>
+
+import header from "./header";
+import navigation from './navigation';
+
 export default {
   data () {
     return {
      
     }
+  },
+  components:{
+  	"list-header": header,
+    "navigation": navigation
   }
 }
 </script>
-
+	
 
 <style scoped>
-	
+	.main-list{
+		position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    min-height: 100%;
+		background: #f5f5f5;
+	}
 </style>
