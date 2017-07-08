57b50b7c42a070f92f4b2e845eb721ef89ab761d
xingchengshanyao <446582654@qq.com>
2017-07-08 10:05:38 +0800

    tijiao footer

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+        <index-footer></index-footer>
+	</div>
+
+</template>
+
+<script>
+
+    import footer from './footer'
+
+
+    export default {
+        created: function(){
+
+        },
+        data () {
+            return {
+
+            }
+        },
+        components: {
+            "index-footer": footer
+
+        }
+    }
+</script>
+
+<style scoped>
+
+
+
+
+</style>
