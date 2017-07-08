17f4939368a84a6e325377bff0c639753f61af74
xingchengshanyao <446582654@qq.com>
2017-07-08 21:42:23 +0800

    ljz

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -4,6 +4,7 @@
         <detail-header :data="swiperInfo"></detail-header>
         <introduction :data="addressInfo"></introduction>
        	<download></download>
+        <detail-comment :data="commentInfo" :dataImg="commentImg"></detail-comment>
         <detail-recommended :data="recommendedInfo"></detail-recommended>
         <detail-footer></detail-footer>
     </div>
@@ -14,6 +15,7 @@
 import header from './header'
 import download from './download'
 import address from './address'
+import comment from './comment'
 import recommended from './recommended'
 import footer from './footer'
 
@@ -21,9 +23,13 @@ export default {
 	created: function(){
 		this.$http.get('static/detail.json').then( response=>{
 			if(response.body.ret){
+                console.log(response.body)
 				this.swiperInfo = response.body.data.swiperInfo;
 				this.addressInfo = response.body.data.addressInfo;
                 this.recommendedInfo= response.body.data.recommendedInfo;
+                this.commentInfo=response.body.data.commentInfo;
+                this.commentImg=response.body.data.commentImg;
+                console.log(this.commentImg)
 			}
 		},response=>{
 			console.log("get detail data error")
@@ -34,13 +40,16 @@ export default {
         return {
             swiperInfo:[],
             addressInfo:[],
-            recommendedInfo:[]
+            recommendedInfo:[],
+            commentInfo:[],
+            commentImg:[]
         }
     },
     components: {
         "detail-header": header,
         "download": download,
         "introduction": address,
+        "detail-comment":comment,
         "detail-recommended":recommended,
         "detail-footer": footer
 	}
