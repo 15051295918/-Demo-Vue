b7e6ba4881570b2bbb46b4f9b778a3a124182416
tudou <hello@163.com>
2017-07-08 09:46:54 +0800

    xiaohehe0708

--- src/pages/onedaytour-detail/header/prdcardCom.vue
+++ src/pages/onedaytour-detail/header/prdcardCom.vue
@@ -1,30 +1,18 @@
 <template>
     <div class="mp-prdcard-con" mp-role="productCardCon">
-        <h3 class="mp-prdcard-name">[北京出发]天安门＋故宫【赠送珍宝馆】＋八达岭长城＋鸟巢·水立方一日游</h3>
+        <h3 class="mp-prdcard-name">{{JiCardCon.prdcardName}}</h3>
         <div class="mp-prdcard-pricecon">
-            <em class="mp-prdcard-priceicon">¥</em>
-            <span class="mp-prdcard-pricetext">228</span>
-            <span class="mp-prdcard-gray">起</span>
-            <span class="mp-bookingahead-text">可订明日</span>
-            <span class="mp-prdcard-supplier">已售:488</span>
+            <em class="mp-prdcard-priceicon">{{JiCardCon.prdcardPriceicon}}</em>
+            <span class="mp-prdcard-pricetext">{{JiCardCon.prdcardPricetext}}</span>
+            <span class="mp-prdcard-gray">{{JiCardCon.prdcardGray}}</span>
+            <span class="mp-bookingahead-text">{{JiCardCon.bookingAheadText}}</span>
+            <span class="mp-prdcard-supplier">{{JiCardCon.prdcardSupplier}}</span>
         </div>
         
         <div class="mp-prdcard-tagcon">
-            <div class="mp-prdcard-tag-item">
-                <span class="mp-prdcard-tag-icon mpg-iconfont"></span>
-                <span class="mp-prdcard-tag-desc">条件退</span>
-            </div>
-            <div class="mp-prdcard-tag-item">
-                <span class="mp-prdcard-tag-icon mpg-iconfont"></span>
-                <span class="mp-prdcard-tag-desc">铁定成团</span>
-            </div>
-            <div class="mp-prdcard-tag-item">
-                <span class="mp-prdcard-tag-icon mpg-iconfont"></span>
-                <span class="mp-prdcard-tag-desc">无自费</span>
-            </div>
-            <div class="mp-prdcard-tag-item">
-                <span class="mp-prdcard-tag-icon mpg-iconfont"></span>
-                <span class="mp-prdcard-tag-desc">无购物</span>
+            <div class="mp-prdcard-tag-item" v-for="item in JiPrdcardTagItem">
+                <span class="mp-prdcard-tag-icon mpg-iconfont">{{item.icon}}</span>
+                <span class="mp-prdcard-tag-desc">{{item.tagDesc}}</span>
             </div>
         </div>
     </div>
@@ -35,7 +23,27 @@
 
         data() {
             return {
-
+                JiCardCon: {
+                    "prdcardName": "[北京出发]天安门＋故宫【赠送珍宝馆】＋八达岭长城＋鸟巢·水立方一日游",
+                    "prdcardPriceicon": "¥",
+                    "prdcardPricetext": "228",
+                    "prdcardGray": "起",
+                    "bookingAheadText": "可订明日",
+                    "prdcardSupplier": "已售:488"
+                },
+                JiPrdcardTagItem: [{
+                    "icon": "",
+                    "tagDesc": "条件退"
+                }, {
+                    "icon": "",
+                    "tagDesc": "铁定成团"
+                }, {
+                    "icon": "",
+                    "tagDesc": "无自费"
+                }, {
+                    "icon": "",
+                    "tagDesc": "无购物"
+                }]
             }
         }
     }
@@ -43,5 +51,83 @@
 
 
 <style scoped>
-
+    .mp-prdcard-con {
+        position: relative;
+        margin-top: -.1rem;
+        padding: .2rem .2rem 0 .2rem;
+        background: #fff;
+        border-radius: .1rem .1rem 0 0;
+    }
+    
+    .mp-prdcard-name {
+        overflow: hidden;
+        display: -webkit-box;
+        color: #212121;
+        font-size: .36rem;
+        line-height: .44rem;
+        text-overflow: ellipsis;
+    }
+    
+    .mp-prdcard-pricecon {
+        overflow: hidden;
+        margin: .16rem 0;
+        height: .48rem;
+        font-size: 0;
+    }
+    
+    .mp-prdcard-priceicon {
+        color: #ff8300;
+        font-size: .24rem;
+    }
+    
+    .mp-prdcard-pricetext {
+        color: #ff8300;
+        font-size: .48rem;
+    }
+    
+    .mp-prdcard-gray {
+        color: #9e9e9e;
+        font-size: .24rem;
+    }
+    
+    .mp-bookingahead-text {
+        color: #616161;
+        font-size: .24rem;
+        margin-left: .2rem;
+    }
+    
+    .mp-prdcard-supplier {
+        color: #616161;
+        float: right;
+        height: .24rem;
+        margin-top: .2rem;
+    }
+    
+    .mp-prdcard-tagcon {
+        overflow: hidden;
+        padding-bottom: .2rem;
+        font-size: 0;
+    }
+    
+    .mp-prdcard-tag-item:not(:last-child) {
+        margin-right: .2rem;
+    }
+    
+    .mp-prdcard-tag-item {
+        float: left;
+        height: .34rem;
+        font-size: .24rem;
+        line-height: .34rem;
+    }
+    
+    .mp-prdcard-tag-icon {
+        margin-right: .04rem;
+        color: #00afc7;
+        font-size: .28rem;
+        vertical-align: -.02rem;
+    }
+    
+    .mp-prdcard-tag-desc {
+        color: #616161;
+    }
 </style>
--- src/pages/onedaytour-detail/header/user-reviews.vue
+++ src/pages/onedaytour-detail/header/user-reviews.vue
@@ -1,29 +1,181 @@
 <template>
     <div>
-        user-revues
+        <div class="mp-commentsinfo-con" style="margin-bottom:0;">
+            <a href="http://touch.piao.qunar.com/touch/weekend/comment.htm?spuId=355112&amp;cat=from_area%3Dts_type_nav%26from_index%3D2%26from_value%3D%25E4%25B8%2580%25E6%2597%25A5%25E6%25B8%25B8%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" class="mpg-linkarea" mp-role="mp-select-comment" data-click="t_comment_top"></a>
+            <span class="mp-commentswell-score">4.9</span>
+            <span class="mp-commentswell-text">分</span>
+            <span class="mp-comments-tagscore">非常满意</span>
+            <span class="mp-comments-totalnum">用户评论(219)&nbsp;
+                <span class="mp-iconfont mp-comments-rightarrow">F</span>
+            </span>
+        </div>
+        
+        <div class="mp-border-top" style="padding: .1rem .2rem;background-color: #fff;">
+            <a href="http://touch.piao.qunar.com/touch/weekend/comment.htm?spuId=355112&amp;cat=from_area%3Dts_type_nav%26from_index%3D2%26from_value%3D%25E4%25B8%2580%25E6%2597%25A5%25E6%25B8%25B8%26dist_city%3D%25E5%258C%2597%25E4%25BA%25AC" class="mpg-linkarea" mp-role="mp-select-comment" data-click="t_comment_top"></a>
+            <div class="mp-comments-item">
+                <div class="mp-content-level">
+                    <span class="mpf-starlevel" style="width:1.5rem;font-size:.2rem;">
+                        <strong class="mpg-iconfont mpf-starlevel-gain" style="width:100%">BBBBB</strong>
+                        <span class="mpg-iconfont mpf-starlevel-total">BBBBB</span>
+                    </span>
+                    <span class="mp-comments-date">o*2&nbsp;&nbsp;2017-07-02</span>
+                </div>
+                <p class="mp-wellcomments-text mp-ellipsis2">一天的行程很满意，小张导游讲解很仔细，没有额外消费，每个景点都很用心，最后快结束还介绍北京的特色给我们，非常好的导游，必须好评～</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    export default {
 
+        data() {
+            return {
 
-export default {
-   
-    data () {
-        return {
-             
+            }
+        },
+        components: {
+
         }
-    },
-    components: {
-        
     }
-}
 </script>
 
 
 <style scoped>
-    div{
-        background: red;
+    .mp-commentsinfo-con {
+        position: relative;
+        margin-top: .2rem;
+        background-color: #fff;
+        height: .8rem;
+        line-height: .8rem;
     }
-
+    
+    .mpg-linkarea {
+        position: absolute;
+        color: #00afc7;
+        z-index: 2;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    
+    .mp-commentswell-score {
+        font-size: .4rem;
+        color: #00afc7;
+        margin-left: .2rem;
+    }
+    
+    .mp-commentswell-text {
+        color: #00afc7;
+    }
+    
+    .mp-comments-tagscore {
+        display: inline-block;
+        margin-left: .1rem;
+        color: #616161;
+    }
+    
+    .mp-comments-totalnum {
+        float: right;
+        position: relative;
+        top: .25rem;
+        display: inline-block;
+        line-height: .28rem;
+        font-size: .28rem;
+        margin-left: .16rem;
+        margin-right: .2rem;
+        color: #9e9e9e;
+    }
+    
+    .mp-comments-rightarrow {
+        font-family: 'mpiconfont';
+        speak: none;
+        font-style: normal;
+        font-weight: normal;
+        font-variant: normal;
+        text-transform: none;
+        line-height: 1;
+    }
+    
+    .mp-border-top {
+        position: relative;
+    }
+    
+    .mpg-linkarea {
+        position: absolute;
+        color: #00afc7;
+        z-index: 2;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    
+    .mp-comments-item:last-child {
+        border-bottom: 0;
+    }
+    
+    .mp-comments-item {
+        padding: .2rem 0!important;
+        padding-top: 0.2rem;
+        padding-right: 0px;
+        padding-bottom: 0.2rem;
+        padding-left: 0px;
+    }
+    
+    .mp-comments-item {
+        color: #616161;
+        padding: .2rem 0;
+        border-bottom: 1px dashed #ccc;
+    }
+    
+    .mpf-starlevel {
+        display: inline-block;
+        position: relative;
+        width: 1.4rem;
+        height: .28rem;
+    }
+    
+    .mpf-starlevel-gain {
+        letter-spacing: .06rem;
+        z-index: 2;
+        color: #00bcd4;
+    }
+    
+    .mpf-starlevel-gain,
+    .mpf-starlevel-total {
+        overflow: hidden;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    
+    .mpf-starlevel-total {
+        color: #e0e0e0;
+        z-index: 1;
+        letter-spacing: .06rem;
+    }
+    
+    .mp-comments-date {
+        float: right;
+        margin-left: .2rem;
+        color: #616161;
+    }
+    
+    .mp-wellcomments-text {
+        display: -webkit-box;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
+    
+    .mp-wellcomments-text.mp-ellipsis2 {
+        line-height: .4rem;
+    }
 </style>
