02986e8e59d83a6962b65c260da78f9f403191b5
yangguohui134638 <943112074@qq.com>
2017-07-10 08:46:34 +0800

    change

--- src/pages/onedaytour-detail/footer/more-quotation.vue
+++ src/pages/onedaytour-detail/footer/more-quotation.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-         更多报价
+          
     </div>
 </template>
         
--- src/pages/onedaytour-detail/footer/user-comment.vue
+++ src/pages/onedaytour-detail/footer/user-comment.vue
@@ -1,55 +1,154 @@
 <template>
     <div class="contain border-leftright">
-    	<h3 class="comment-title border-bottom"><span class="title-leftline"></span>用户评论</h3>
-        <div class="content">
+    	<h3 class="comment-title border-bottom"><span class="title-leftline"></span>{{data.title}}</h3>
+        <div class="content" v-for="(list, index) in data.lists" :key="index + 'content_'">
         	<div class="content-level">
-        		<p class="content-starlevel">
-        			<span class="iconfont star">&#xe604;</span>
-                    <span class="iconfont star">&#xe604;</span>
-        			<span class="iconfont star">&#xe604;</span>
-        			<span class="iconfont star">&#xe604;</span>
-        			<span class="iconfont star">&#xe604;</span>
-        		</p>
-        		<p class="content-date">Z*9&nbsp;&nbsp;2017-06-23</p>
+                    <p class="content-graystar">
+                        <span class="iconfont graystar" v-for="item in list.contentStar" v-html="item.icon"></span>
+                        
+                    </p>
+                <div class="contain-star">   
+            		<p class="content-starlevel">
+            			<span class="iconfont star" v-for="item in list.contentStar" v-html="item.icon"></span>
+            		</p>
+                </div> 
+        		<p class="content-date">{{list.name}}&nbsp;&nbsp;{{list.date}}</p>
          	</div>
         	 <p class="content-text">
-        	 	我是第一次报一天游的，在沒有报前总覺得不好，但旭冉旅遊团的小凡称小格格，真的值得一赞，剛好是北京下暴雨的一天，我們往八达岭长城开去，已經差不多到
-        	 	，才收到全封閉通知，小格格还幫我們全去了幾個售票处问问可以上去长城吗，這点小格格很细心，在讲解途中，很全面很细致，真的值得赞！下次來北京，一定找你小格格做導遊！
+        	 	{{list.contentText}}
         	 </p>
-                <div class="contain-imgs">
-                	 <div class="comment-imgs">
-            	 		<img class="comment-img comment-img-first" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-            	 		<img class="comment-img" src="http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg" />
-                	 </div>
-                 </div>    
+            <div class="contain-imgs">
+            	 <div class="comment-imgs">
+        	 		<img class="comment-img" v-for="(item, index) in list.commentImg" :src="item.src" :class="{'comment-img-first' : index == 0}" />
+        	 	 
+            	 </div>
+             </div>    
         </div>
-        <div class="more-comment">查看更多评论 
-        	<span class="iconfont">&#xe768;</span>
+        <div class="more-comment">
+                {{data.last.comment}}
+        	<span class="iconfont" v-html="data.last.icon"></span>
         </div>
     </div>
 </template>
         
 <script>
-
-export default {
+ export default {
    
     data () {
         return {
         	"ret": true,
              "data": {
+                "title": "用户评论",
+                "lists": [
+                    {
+                         "contentStar": [ 
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            }
+                         ],
+                         "name": "Z*9",  
+                         "date": "2017-06-23",  
+                        "contentText":  "我是第一次报一天游的，在沒有报前总覺得不好，但旭冉旅遊团的小凡称小格格，真的值得一赞，剛好是北京下暴雨的一天，我們往八达岭长城开去，已經差不多到，才收到全封閉通知，小格格还幫我們全去了幾個售票处问问可以上去长城吗，這点小格格很细心，在讲解途中，很全面很细致，真的值得赞！下次來北京，一定找你小格格做導遊！"
+                         ,
+                        "commentImg": [
+                            {
+                             "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                            { 
+                                "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                            { 
+                                "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                            { 
+                                "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                            { 
+                                "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                             {
+                                "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                            {
+                                "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            },
+                            {    "src": "http://img1.qunarzz.com/piao/fusion/1706/98/19c9b9f677ebf902.jpg_240x240_5dea1774.jpg"
+                            }
 
+                        ]
+                    },
+                   { 
+                        "contentStar": [ 
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            },
+                            {
+                                "icon": "&#xe604;"
+                            }
+                         ],
+                        "name":   "古*忆" ,
+                        "date":  "2017-01-22",  
+                        "contentText": "八哥人不错。故宫电子票，学生证不半价，给退的40块钱，我自己半价买的。八达岭的滑车票，是八哥帮大家跑腿买的。整个途中讲解很好，没有乱收费，也没有购物。去的是真长城?。一天去了很多地方，还吃了庆丰包子。虽然时间不长，但很有意思。"
+                        ,
+                        "commentImg": [
+                            {
+                                "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },
+                            { 
+                                "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },
+                            {
+                                 "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },
+                             {
+                                "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },{ 
+                                "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },
+                            {
+                                 "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },
+                             {
+                                "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            },
+                            {
+                                "src": "http://img1.qunarzz.com/piao/fusion/1701/dd/4c9d98923c305602.jpg_240x240_2c78a148.jpg"
+                            }
+                          ]
+                    } 
+                ],       
+             "last": {
+                "comment": "查看更多评论",
+                "icon": "&#xe768;"
              }
+               
+         }
+           
         }
     },
     components: {
-        
+         
     }
 }
 </script>
@@ -85,19 +184,33 @@ export default {
         padding: 0 .24rem;
     }
     .content-level{
+        position: relative;
         font-size: 0;
         height: .42rem;
         padding-top: .22rem;
-
+    }
+    .contain-star{
+        position: absolute;
+        left: 0;
+        top: .22rem;
+        width: 1.54rem;
+        height: 0.25rem;
+        overflow: hidden;
     }
     .content-starlevel{
-        float: left;
-
+         width: 2rem;
+         height: .25rem;
+    }
+    .content-graystar{
+         float: left;
     }
     .star{
         font-size: .28rem;
         color: #00bcd4;
-        margin-right: .04rem;
+    }
+    .graystar{
+        font-size: .28rem;
+        color: #e0e0e0;
     }
     .content-date{
         float: right;
