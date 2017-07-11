203cb0a17177d2cc0813d4e903f2e82d3dbf7a0d
huangzeyu <506551239@qq.com>
2017-07-11 15:28:29 +0800

    change

--- src/pages/common-detail/detail_header.vue
+++ src/pages/common-detail/detail_header.vue
@@ -1,19 +1,18 @@
 <template>
 	<div class=detail-wrap>
-		<div class="detail-header" @click="handleClick" v-for="itemOne in title_data">
+		<div class="detail-header" @click="handleClick" v-for="itemOne in titleData">
 			<img class="detailheader-img" :src="itemOne.picUrl" />
 			<div class="detailheader-title">
 				<span class="title">{{itemOne.name}}</span>
 			</div>
 			<div class="total-img">
-				<span class="iconfont icon-img" >&#xe634;</span>{{swiper_data.length}}
+				<span class="iconfont icon-img" >&#xe634;</span>{{swiperData.length}}
 			</div>
 		</div>
-		<!--mask-->
 		<div class="detailheader-mask" v-show="seen">
 			<div class="swiper-wrap">
 				<swiper :options="swiperOption" ref="mySwiper">
-			        <swiper-slide v-for="(itemTwo,index) in swiper_data" :key="index + 'detail_img'">
+			        <swiper-slide v-for="(itemTwo,index) in swiperData" :key="index + 'detail_img'">
 			        	<div class="img-wrapper">
 			        		<img class="swiper-img" :src="itemTwo.imgUrl"/>
 			        	</div>
@@ -31,13 +30,12 @@
 	       	</div>
 	       	<div class="swiper-pagination" slot="pagination"></div>
 		</div>
-		<!--mask结束-->
 	</div>
 </template>
 
 <script>
 	export default {
-		data(){
+		data() {
 		 	return{
 		 		seen:false,
 		 		swiperOption: {
@@ -52,8 +50,10 @@
 	        	}
 		 	}
 		 },
-		 props:["title_data","swiper_data"],
-		 methods:{
+		 
+		 props:["titleData","swiperData"],
+		 
+		 methods: {
 		 	handleClick: function() {
 		 		return this.seen = true;
 		 	}
@@ -67,89 +67,89 @@
 	.detail-header{
 		position: relative;
 		overflow: hidden;
-		width:100%;
-		height:0;
+		width: 100%;
+		height: 0;
 		padding-bottom: 55%;
 	}
 	.detailheader-img{
-		width:100%;
+		width: 100%;
 		vertical-align: top;
 	}
 	.detailheader-title{
-		position:absolute;
-		bottom:.26rem;
-		left:.2rem;
+		position: absolute;
+		bottom: .26rem;
+		left: .2rem;
 	}
 	.title{
 		font-size: .32rem;
-		color:#fff;
+		color: #fff;
 	}
 	.total-img{
 		position: absolute;
-		bottom:.26rem;
-		right:.2rem;
-		width:1.2rem;
-		height:.4rem;
-		background:rgba(0,0,0,0.5);
+		bottom: .26rem;
+		right: .2rem;
+		width: 1.2rem;
+		height: .4rem;
+		background: rgba(0,0,0,0.5);
 		border-radius: .2rem;
 		font-size: .24rem;
-		color:#fff;
+		color: #fff;
 		text-align: center;
 		line-height: .4rem;
 	}
 	.detailheader-mask{
 		position: fixed;
-		top:0;
+		top: 0;
 		left: 0;
-		bottom:0;
-		right:0;
-		background:#000;
-		color:#fff;
-		z-index:99;
+		bottom: 0;
+		right: 0;
+		background: #000;
+		color: #fff;
+		z-index: 99;
 	}
 	.swiper-wrap{
-		width:100%;
-		height:100%;
-		margin-top:2.9rem;
+		width: 100%;
+		height: 100%;
+		margin-top: 2.9rem;
 	}
 	.img-wrapper{
 		overflow: hidden;
-		width:100%;
-		height:0;
-		padding-bottom:100%;
+		width: 100%;
+		height: 0;
+		padding-bottom: 100%;
 	}
 	.swiper-img{
 		width: 100%;
 	}
 	.mask-close{
-		position:absolute;
-		top:1.5rem;
-		right:.4rem;
-		width:.5rem;
-		height:.5rem;
+		position: absolute;
+		top: 1.5rem;
+		right: .4rem;
+		width: .5rem;
+		height: .5rem;
 		border-radius: 50%;
 		text-align: center;
 		background: rgba(51,51,51,.8);
-		color:#fff;
+		color: #fff;
 		line-height: .5rem;
 		font-size: .4rem;
 	}
 	.swiper-wrap .swiper-button-prev,.swiper-wrap .swiper-button-next{
-		top:88%;
-		width:.8rem;
-		height:.8rem;
+		top: 88%;
+		width: .8rem;
+		height: .8rem;
 		border-radius: 50%;
-		background:rgba(51,51,51,.8);
-		line-height:.8rem;
-		text-align:center;
+		background: rgba(51,51,51,.8);
+		line-height: .8rem;
+		text-align: center;
 		font-weight: bold;
 	}
 	.detailheader-mask .swiper-pagination{
-		bottom:1.25rem;
+		bottom: 1.25rem;
 		left:0;
 	}
 	.icon-img{
-		margin-right:.05rem;
+		margin-right: .05rem;
 	}
 	.icon-close{
 		font-size: .52rem;
