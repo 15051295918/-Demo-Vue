20b3740a9a06c036c154ab8baa85731275e4245c
ranran <17839227563@163.com>
2017-07-14 11:11:09 +0800

    change

--- src/pages/index/ads.vue
+++ src/pages/index/ads.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="ads border-topbottom">
 		<div class="ads-item border-right" v-for="(item, index) in adsInfo" :key="index + '_ads_item'">
-			<router-link to="/sales/">
+			<router-link v-bind:to="{path: item.link}">
 				<img class="ads-img" :src="item.imgUrl">
 			</router-link>
 		</div>
@@ -15,10 +15,10 @@ export default {
      	adsInfo:[
      		{
      			imgUrl: "http://img1.qunarzz.com/piao/fusion/1601/29/30427c0e0658b5f7.png",
-     			link: ""
+     			link: "/oneday"
      		},{
      			imgUrl: "http://img1.qunarzz.com/piao/fusion/1601/29/30427c0e0658b5f7.png",
-     			link: ""
+     			link: "/campaign"
      		}
      	]
     }
