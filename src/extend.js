c1a94c556670d91f88d30f69067a001d598ddedb
wuhe <396834321@qq.com>
2017-07-14 22:38:51 +0800

    change

--- src/extend.js
+++ src/extend.js
@@ -3,8 +3,8 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 import VueResource from 'vue-resource'
 import FastClick from 'fastclick'
 import Vuex from 'vuex';
-import AMap from 'vue-amap'
 import VueLazyload from 'vue-lazyload';
+import BaiduMap from 'vue-baidu-map';
 
 Vue.config.productionTip = false
 
@@ -18,11 +18,10 @@ Vue.use(VueLazyload, {
   attempt: 1
 })
 
-Vue.use(AMap);
-AMap.initAMapApiLoader({
-  key: '2ff2e5535cd7ae698e7af431e6558948',
-  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
-});
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: 'ItGUcMmiA6r3XESMFiE6oLM0NtdWZIqf'
+})
 
 FastClick.attach(document.body);
 
